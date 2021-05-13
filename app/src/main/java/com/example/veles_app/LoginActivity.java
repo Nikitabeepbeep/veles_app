46936280985b57c461b448cb34fc07e5b06bac6b
Nikita <nekekos@yandex.ru>
2021-05-13 13:06:42 +0500

    + - Работающая регистрация/вход

--- app/src/main/java/com/example/veles_app/LoginActivity.java
+++ app/src/main/java/com/example/veles_app/LoginActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginBtn;
     private EditText usernameInput, phoneInput, passwordInput;
     private CheckBox checkBoxRememberMe;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +26,28 @@ public class LoginActivity extends AppCompatActivity {
         phoneInput = (EditText) findViewById(R.id.login_phone_input);
         passwordInput = (EditText) findViewById(R.id.login_password_input);
         checkBoxRememberMe = (CheckBox) findViewById(R.id.login_checkbox);
-try {
-    loginBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent login_btnIntent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(login_btnIntent);
-        }
-    });
-} catch (Exception e) {
-    System.out.println("Произошла ошибка");
-}
+        DB = new DBHelper(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = phoneInput.getText().toString();
+                String pass = passwordInput.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Пожалуйста, заполните все поля!", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.check_password(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(LoginActivity.this, "Успешный вход!", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Неверные данные!Пожалуйста, проверьте правильность ввода!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/veles_app/RegisterActivity.java
+++ app/src/main/java/com/example/veles_app/RegisterActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button registerBtn;
-    private EditText usernameInput, phoneInput, passwordInput;
+    private EditText usernameInput, passwordInput, passwordInput1;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +24,39 @@ public class RegisterActivity extends AppCompatActivity {
 
         registerBtn = (Button) findViewById(R.id.register_btn);
         usernameInput = (EditText) findViewById(R.id.register_username_input);
-        phoneInput = (EditText) findViewById(R.id.register_phone_input);
         passwordInput = (EditText) findViewById(R.id.register_password_input);
+        passwordInput1 = (EditText) findViewById(R.id.register_password_input1);
+        DB = new DBHelper(this);
 
-        try {
-            registerBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent registerBtnIntent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(registerBtnIntent);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String password1 = passwordInput1.getText().toString();
+                if (user.equals("") || password.equals("") || password1.equals(""))
+                    Toast.makeText(RegisterActivity.this, "Пожалуйста, заполните все поля!", Toast.LENGTH_SHORT).show();
+                else {
+                    if (password.equals(password1)) {
+                        Boolean checkuser = DB.check_username(user);
+                        if (checkuser == false) {
+                            Boolean insert = DB.insertData(user, password);
+                            if (insert == true) {
+                                Toast.makeText(RegisterActivity.this, "Успешная регистрация!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Ошибка регистрации!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Пользователь с таким именем уже существует,пожалуйста,выберите другое имя!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Пароли не совпадают!", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            });
-        } catch (Exception e) {
-            System.out.println("Произошла ошибка");
-        }
+            }
+        });
     }
 }
